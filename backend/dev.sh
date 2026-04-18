c03e5a3e430a11c780cb69022018465685335956
Chris Register <VBWizard@Live.com>
2026-04-18 19:33:46 -0400

    Update start script and Suggestions default prompt

--- backend/dev.sh
+++ backend/dev.sh
@@ -1,3 +1,5 @@
 export CORS_ALLOW_ORIGIN="http://localhost:5173;http://localhost:8080"
 PORT="${PORT:-8080}"
-uvicorn open_webui.main:app --port $PORT --host 0.0.0.0 --forwarded-allow-ips "${FORWARDED_ALLOW_IPS:-*}" --reload
+uvicorn open_webui.main:app --port $PORT --host 0.0.0.0 --forwarded-allow-ips "${FORWARDED_ALLOW_IPS:-*}" 
+# CLR 03/18/2026: Removed reload. Causes 100% cpu usage constantly
+# --reload
